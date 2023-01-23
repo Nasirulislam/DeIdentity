7694e2f6babdf7c13ce821f18055504861e06fda
Haider <haiwolf777@gmail.com>
2023-01-23 11:54:53 +0500

    Update pages

--- src/components/provider/ServiceProvider.js
+++ src/components/provider/ServiceProvider.js
@@ -1,9 +1,61 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 
 const ServiceProvider = () => {
   return (
-    <div>
-      
+    <div className='container mt-5'>
+    <div class="form-check my-2">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+    <label class="form-check-label" for="flexCheckDefault">
+     ID
+    </label>
+  </div>
+  <div class="form-check my-2">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
+    <label class="form-check-label" for="flexCheckChecked">
+     Name
+    </label>
+  </div>
+  <div class="form-check my-2">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
+  <label class="form-check-label" for="flexCheckChecked">
+   CNIC
+  </label>
+</div>
+<div class="form-check my-2">
+<input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
+<label class="form-check-label" for="flexCheckChecked">
+  Address
+</label>
+</div>
+
+<form action="/action_page.php" className='form-element'>
+
+<div className='d-flex justify-content-center flex-column  mt-4'>
+<label for="id" className=' label-text '>ID:</label>
+<input type="text" id="fname" name="id" className='input-feild' placeholder='ID'/>
+</div>
+<div className='d-flex justify-content-center flex-column mt-4'>
+<label for="fname" className=' label-text '>Full Name:</label>
+<input type="text" id="fname" name="fname" className='input-feild' placeholder='Name'/>
+</div>
+<div className='d-flex justify-content-center flex-column mt-4'>
+<label for="cnic" className=' label-text '>CNIC:</label>
+<input type="text" id="fname" name="cnic" className='input-feild' placeholder='CNIC'/>
+</div>
+<div className='d-flex  mt-4 flex-column'>
+<label for="address" className=' label-text'>Address:</label>
+<input type="text" id="fname" name="address" className='input-feild' placeholder='Address'/>
+</div>
+
+
+
+
+
+<div className='d-flex justify-content-center mt-3'>
+<Button className='mx-5'>Service</Button>
+</div>
+</form>
     </div>
   )
 }
--- src/components/tokens/VerifyToken.js
+++ src/components/tokens/VerifyToken.js
@@ -1,7 +1,12 @@
-import React from 'react'
 import { Button } from 'react-bootstrap'
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
 
 const VerifyToken = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
    
@@ -17,23 +22,49 @@ const VerifyToken = () => {
     <div className='form container'>
     <form action="/action_page.php" className='form-element'>
 
+    <div className='d-flex justify-content-center flex-column  mt-4' onClick={handleShow}>
+    <label for="id" className=' label-text '>Address:</label>
+    <textarea style={{resize: 'none', outline: 'none', borderRadius:'8px' }} id="token-verify" name="token" rows="2" cols="50">They offer free tutorials in all web development technologies.</textarea>
+    </div>
+
+    <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Tokens</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+
+        <div className=' container'>
+    <form action="/action_page.php" className='form-element'>
+
     <div className='d-flex justify-content-center flex-column  mt-4'>
     <label for="id" className=' label-text '>ID:</label>
-    <textarea style={{resize: 'none', outline: 'none', borderRadius:'8px' }} id="token-verify" name="token" rows="2" cols="50">They offer free tutorials in all web development technologies.</textarea>
+    <input type="text" id="fname" name="id" className='input-feild'/>
     </div>
     <div className='d-flex justify-content-center flex-column mt-4'>
     <label for="fname" className=' label-text '>Full Name:</label>
-    <textarea style={{resize: 'none', outline: 'none', borderRadius:'8px' }} id="token-verify" name="token" rows="2" cols="50">They offer free tutorials in all web development technologies.</textarea>
+    <input type="text" id="fname" name="fname" className='input-feild' />
     </div>
     <div className='d-flex justify-content-center flex-column mt-4'>
     <label for="cnic" className=' label-text '>CNIC:</label>
-    <textarea style={{resize: 'none', outline: 'none', borderRadius:'8px' }} id="token-verify" name="token" rows="2" cols="50">They offer free tutorials in all web development technologies.</textarea>
+    <input type="text" id="fname" name="cnic" className='input-feild' />
     </div>
-    <div className='d-flex  mt-4 flex-column'>
+    <div className='d-flex  my-4 flex-column'>
     <label for="address" className=' label-text'>Address:</label>
-    <textarea style={{resize: 'none', outline: 'none', borderRadius:'8px' }} id="token-verify" name="token" rows="2" cols="50">They offer free tutorials in all web development technologies.</textarea>
+    <input type="text" id="fname" name="address" className='input-feild' />
     </div>
   </form>
+    </div>
+        
+        </Modal.Body>
+       
+      </Modal>
+
+
+
+  </form>
+  
+
+  
     </div>
     </div>
 
